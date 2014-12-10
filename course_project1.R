0da0ee611e8c650eeae543db7c879f33c9defa82
Adonis Caballero <adonis.caballero2010@yahoo.com>
2014-12-10 14:18:26 +0800

    Adding HelloWord.md to the repo

---
+++ course_project1.R
@@ -0,0 +1,9 @@
+# Coursera
+# Data Science Specilization Track
+# Data Scientist's Toolbox - Course Project
+
+# Instructions
+# Install R 
+# Install Rstudio
+# Open Rstudio and take a screenshot
+# Submit a screenshot of Rstudio open on your screen using one of these formats: png, jpg, gif, pdf
